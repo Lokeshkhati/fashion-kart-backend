5cab2bbba9fd61fadc4749e66412834bf6a58939
lokesh <lokeshkhati005@gmail.com>
2023-06-08 14:39:44 +0530

    fix token

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ import wishlist from './routes/wishlist.js'
 
 config()
 app.use(express.json())
-app.use(cors({ origin: " http://localhost:5173", credentials: true }))
+app.use(cors({ origin: " http://localhost:5173", credentials: true, optionSuccessStatus: 200 }))
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
